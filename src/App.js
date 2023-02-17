fc06634cced1f623eec0aa58bd06cf5565413309
mavu1707 <markus.yung.vullum@gmail.com>
2023-02-17 11:35:50 +0100

    Lagt til components, mangler å legge til router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import './css/main.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>test</h1>
+    <div>
+      <header>{Header}</header>
+    </div>
   );
 }
 
